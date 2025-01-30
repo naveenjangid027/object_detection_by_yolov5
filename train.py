31934253dea26143825c09699e8e4d8e533e85d2
naveenjangid027 <147740082+naveenjangid027@users.noreply.github.com>
2025-01-30 10:47:08 +0530

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ Models:     https://github.com/ultralytics/yolov5/tree/master/models
 Datasets:   https://github.com/ultralytics/yolov5/tree/master/data
 Tutorial:   https://docs.ultralytics.com/yolov5/tutorials/train_custom_data
 """
-
+# trainpy file in the YOLOv5 in the python programming 
 import argparse
 import math
 import os
